3856a08d6a3932e4188f23a13f80e57044e2ff8f
fransjr <francesc@xceed.me>
2020-11-12 12:43:23 +0100

    Adds php unit xml and some tests

--- tests/Domain/City/ValueObject/CityCoordinatesTest.php
+++ tests/Domain/City/ValueObject/CityCoordinatesTest.php
@@ -5,8 +5,34 @@ namespace Domain\City\ValueObject;
 
 
 use PHPUnit\Framework\TestCase;
+use Zinio\Cesc\Domain\City\Exception\InvalidValueException;
+use Zinio\Cesc\Domain\City\ValueObject\CityCoordinates;
 
 class CityCoordinatesTest extends TestCase
 {
+
+    private const GOOD_LATITUDE = 30.23;
+    private const BAD_LATITUDE = 100.23;
+    private const GOOD_LONGITUDE = -100.23;
+    private const BAD_LONGITUDE =  -200.43;
+
+    public function testBadLatitude():void
+    {
+        self::expectException(InvalidValueException::class);
+        new CityCoordinates(self::BAD_LATITUDE, self::GOOD_LONGITUDE);
+    }
+
+    public function testBadLongitude():void
+    {
+        self::expectException(InvalidValueException::class);
+        new CityCoordinates(self::GOOD_LATITUDE, self::BAD_LONGITUDE);
+    }
+
+    public function testGetters():void
+    {
+        $coordinates = new CityCoordinates(self::GOOD_LATITUDE, self::GOOD_LONGITUDE);
+        self::assertEquals(self::GOOD_LATITUDE, $coordinates->getLatitude());
+        self::assertEquals(self::GOOD_LONGITUDE, $coordinates->getLongitude());
+    }
 
 }
--- tests/Domain/City/ValueObject/CityNameTest.php
+++ tests/Domain/City/ValueObject/CityNameTest.php
@@ -5,8 +5,16 @@ namespace Domain\City\ValueObject;
 
 
 use PHPUnit\Framework\TestCase;
+use Zinio\Cesc\Domain\City\ValueObject\CityName;
 
 class CityNameTest extends TestCase
 {
+    private const TEST_NAME = 'test';
+
+    public function testGetter(): void
+    {
+        $cityName = new CityName(self::TEST_NAME);
+        self::assertEquals(self::TEST_NAME, $cityName->value());
+    }
 
 }
