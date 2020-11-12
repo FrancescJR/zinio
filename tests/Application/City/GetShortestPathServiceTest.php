0fb4152b3d3407e6186377b1ca8a082c40d30f52
fransjr <francesc@xceed.me>
2020-11-12 13:49:48 +0100

    finish all tests

--- tests/Application/City/GetShortestPathServiceTest.php
+++ tests/Application/City/GetShortestPathServiceTest.php
@@ -5,8 +5,26 @@ namespace Application\City;
 
 
 use PHPUnit\Framework\TestCase;
+use Zinio\Cesc\Application\City\GetShortestPathService;
+use Zinio\Cesc\Application\City\PlainObject\CityPO;
+use Zinio\Cesc\Domain\City\City;
+use Zinio\Cesc\Domain\City\Service\CreateCitiesService;
 
 class GetShortestPathServiceTest extends TestCase
 {
+
+    public function testExecute(): void
+    {
+        $city = self::createMock(City::class);
+        $createCitiesService = self::createMock(CreateCitiesService::class);
+        $createCitiesService->method('execute')->willReturn([$city]);
+
+        $service = new GetShortestPathService($createCitiesService);
+
+        $result = $service->execute([]);
+
+        self::assertCount(1, $result);
+        self::assertEquals(CityPO::class, get_class($result[0]));
+    }
 
 }
